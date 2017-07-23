c017b53a58f009dfec6de6586c46cb99431ea4d1
julien.bernard.iphone <julien.bernard.iphone@gmail.com>
2017-07-23 15:05:11 -0400

    sma unittested

--- source/indicators.d
+++ source/indicators.d
@@ -17,40 +17,56 @@ pure ElementType!R1 computeFilter(R1, R2, R3, F)
     return initialValue;
   }
   else {
-    auto FIR = dotProduct(input[currentIndex - zeros[currentIndex].length + 1 .. currentIndex + 1], zeros[currentIndex]);
-    auto IIR = dotProduct(iota(poles[currentIndex].length)
-                          .map!((i) => computeFilter(input, zeros, poles, currentIndex - i - 1, neededOffset, initialValue)),
-                          poles[currentIndex]);
+    auto result = dotProduct(input[currentIndex - zeros[currentIndex].length + 1 .. currentIndex + 1], zeros[currentIndex]);
     
-    return FIR + IIR;
+    if(poles.length > 0)
+    {
+      result += dotProduct(iota(poles[currentIndex].length)
+                           .map!((i) => computeFilter(input, zeros, poles, currentIndex - i - 1, neededOffset, initialValue)),
+                           poles[currentIndex]);
+    }
+    
+    return result;
   }
 }
 
 unittest
 {
-  import std.algorithm: equal;
+  import std.algorithm: equal, sum;
   import std.math: approxEqual;
+  import std.stdio;
   
   double[] values = [0., 1., 2., 3.];
   auto timestamps = iota(values.length);
   
   // EMA
-  auto zeros = timestamps.map!((a) => [.05]);
-  auto poles = timestamps.map!((a) => [.95]);
-  auto results = timestamps.map!((a) => computeFilter(values, zeros, poles, a, 1, values[0]));
-  
-  import std.stdio;
-  writeln(results);
+  auto emaZeros = timestamps.map!((a) => [.05]);
+  auto emaPoles = timestamps.map!((a) => [.95]);
+  auto emaResults = timestamps.map!((a) => computeFilter(values, emaZeros, emaPoles, a, 1, values[0]));
   
   foreach(i; timestamps)
   {
     if(i > 0)
     {
-      assert(approxEqual(.05*values[i] + .95*results[i-1], results[i]));
+      assert(approxEqual(.05*values[i] + .95*emaResults[i-1], emaResults[i]));
     }
     else 
     {
-      assert(approxEqual(values[i], results[i]));
+      assert(approxEqual(values[i], emaResults[i]));
+    }
+  }
+  
+  // SMA
+  auto smaZeros = timestamps.map!((a) => [1./3., 1./3., 1./3.]);
+  auto smaPoles = timestamps.map!((a) => [0]);
+  auto smaResults = timestamps.map!((a) => computeFilter(values, smaZeros, smaPoles, a, 2, 0.));
+  
+  foreach(i; timestamps)
+  {
+    if(i > 1)
+    {
+      auto mean = sum(values[i - 2 .. i + 1])/3.;
+      assert(approxEqual(mean, smaResults[i]));
     }
   }
 }
