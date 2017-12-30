91043488549c8527b59649a954108649291951df
julien.bernard.iphone <julien.bernard.iphone@gmail.com>
2017-12-30 06:24:02 -0500

    control function

--- source/indicators.d
+++ source/indicators.d
@@ -66,52 +66,54 @@ pure auto sma(Range)(Range input, size_t depth) if (isInputRange!Range)
   assert(approxEqual(sma2.poles(0), [0.]));
 }
 
-template computeFilter(alias indicator, Range) if (isInputRange!Range)
+pure double delegate(size_t) computeFilter(alias indicator, Range)(Range input)
+if (isInputRange!Range 
+    && is(typeof(indicator.neededOffset())) 
+    && is(typeof(indicator.initialValue()))
+    && is(typeof(indicator.zeros(0)))
+    && is(typeof(indicator.poles(0)))) 
 {
-  pure double delegate(size_t) computeFilter(Range input)
-  {
-    return (size_t i) {
+  return (size_t i) {
 
-      // In initialisation period we return null
-      if(i < indicator.neededOffset() - 1)
+    // In initialisation period we return null
+    if(i < indicator.neededOffset() - 1)
+    {
+      return double.nan;
+    }
+    // At the end of initialisation period we return initial value provided
+    else if (i == indicator.neededOffset() - 1)
+    {
+      return indicator.initialValue();
+    }
+    // We compute filter current value
+    else
+    {
+      // Zeros part: I(t)*a0 + I(t-1)*a1 + ... + I(t-n)*an
+      auto result = dotProduct(input[i - indicator.zeros(i).length + 1 .. i + 1], indicator.zeros(i));
+
+      // Check if filter is FIR only
+      if(sum(indicator.poles(i)) == 0.)
       {
-        return double.nan;
+        // Filter is FIR no computation needed
+        result += 0.;
       }
-      // At the end of initialisation period we return initial value provided
-      else if (i == indicator.neededOffset() - 1)
-      {
-        return indicator.initialValue();
-      }
-      // We compute filter current value
       else
       {
-        // Zeros part: I(t)*a0 + I(t-1)*a1 + ... + I(t-n)*an
-        auto result = dotProduct(input[i - indicator.zeros(i).length + 1 .. i + 1], indicator.zeros(i));
+        // Create delegate function to call computeFilter recursively
+        auto _computeFilter = computeFilter!(indicator)(input);
 
-        // Check if filter is FIR only
-        if(sum(indicator.poles(i)) == 0.)
-        {
-          // Filter is FIR no computation needed
-          result += 0.;
-        }
-        else
-        {
-          // Create delegate function to call computeFilter recursively
-          auto _computeFilter = computeFilter!(indicator)(input);
+        // range of indexes
+        auto indexes = iota(indicator.poles(i).length);
 
-          // range of indexes
-          auto indexes = iota(indicator.poles(i).length);
+        // get last filter values
+        auto lastFilterValues = map!((k) => _computeFilter(i-k-1))(indexes);
 
-          // get last filter values
-          auto lastFilterValues = map!((k) => _computeFilter(i-k-1))(indexes);
-
-          // Poles Part: O(t-1)*b1 + O(t-2)*b2 + ... + O(t-n)*bn
-          result += dotProduct(lastFilterValues, indicator.poles(i));
-        }
-        return result;
+        // Poles Part: O(t-1)*b1 + O(t-2)*b2 + ... + O(t-n)*bn
+        result += dotProduct(lastFilterValues, indicator.poles(i));
       }
-    };
-  }
+      return result;
+    }
+  };
 }
 
 @system unittest
@@ -126,7 +128,7 @@ template computeFilter(alias indicator, Range) if (isInputRange!Range)
   auto ema005 = ema(values, 0.05);
   auto computeEma005 = computeFilter!(ema005)(values);
   auto emaResults = map!(computeEma005)(timestamps);
-
+  
   foreach(i; timestamps)
   {
     if(i > 0)
