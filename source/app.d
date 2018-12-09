8e106dbd9291663b846c8664b10c93674fa1b2c7
Julien Bernard <julien.bernard.iphone@gmail.com>
2018-12-09 17:59:12 +0000

    add mir-random dependency

--- source/app.d
+++ source/app.d
@@ -1,6 +1,35 @@
 import std.stdio;
+import std.datetime: Date;
+import std.algorithm: map;
+import mir.random;
+import mir.random.variable: normalVar;
+import mir.random.algorithm: randomSlice;
+import heartbeat: naiveDailyCalendar;
 
 void main()
 {
-	writeln("Done");
+	auto gen = Random(unpredictableSeed);
+	auto d = normalVar;
+
+	double delegate(string) prices = (string t) { return d(gen); };
+	double delegate(string) portfolio = (string t) { return 1000.0; };
+
+	bool signals(string t, double delegate(string) prices, double delegate(string) portfolio)
+	{
+		if(portfolio(t) < 1000.0) return false;
+		
+		if(prices(t) > 0) return true;
+		return false;
+	}
+
+	bool randomSignals(string t)
+	{
+		return signals(t, prices, portfolio);
+	}
+
+	auto cal = naiveDailyCalendar(Date(2000, 1, 3), Date(2018, 10, 31));
+	import std.array;
+	auto result = cal.map!randomSignals;
+	writeln(result.front);
+	
 }
