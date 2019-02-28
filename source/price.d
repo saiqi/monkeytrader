4c8caa1ba5d5d9377ae16c31b43090b9fb1d4ef1
Julien Bernard <julien.bernard.iphone@gmail.com>
2019-02-28 21:42:46 +0000

    random prices

---
+++ source/price.d
@@ -0,0 +1,88 @@
+module price;
+
+import std.range: ElementType, isInfinite, isInputRange;
+import std.traits: isFloatingPoint;
+import mir.random;
+import mir.random.variable: NormalVariable;
+
+///
+enum DataType {OPEN, HIGH, LOW, CLOSE, VOLUME, OPEN_INTEREST}
+
+///
+auto getGaussianPrices(R, T = double)(R calendar, const T mu, const T sigma, const T initialPrice) pure
+if(isFloatingPoint!T)
+{
+    static struct GaussianPrices(R, T)
+    {
+        private T mu_;
+        private T sigma_;
+        private T initialPrice_;
+        private T currentReturn_;
+        private R calendar_;
+        private ElementType!R currentTimestamp_;
+        private size_t currentIndex_;
+
+        this(R calendar, const T mu, const T sigma, const T initialPrice) pure
+        {
+            calendar_ = calendar;
+            mu_ = mu;
+            sigma_ = sigma;
+            initialPrice_ = initialPrice;
+            currentReturn_ = 0.;
+        }
+
+        private auto nextReturn() const
+        {
+            auto d = NormalVariable!double(mu_, sigma_);
+            return d(rne);
+        }
+
+        @property bool empty() const
+        {
+            static if (!isInfinite!R) {
+                return calendar_.length == currentIndex_;
+            } else {
+                return false;
+            }
+        }
+
+        @property auto front() const
+        {
+            assert(!empty);
+            return initialPrice_ * (1 + currentReturn_);
+        }
+
+        void popFront()
+        {
+            currentReturn_ = nextReturn();
+            currentIndex_++;
+        }
+    }
+    return GaussianPrices!(R, T)(calendar, mu, sigma, initialPrice);
+}
+
+unittest {
+    import std.traits: ReturnType;
+    import heartbeat: NaiveCalendar;
+    import std.datetime: Date;
+
+    alias R = ReturnType!(getGaussianPrices!(NaiveCalendar!(Date, "months"), double));
+    static assert(isInputRange!R);
+}
+
+
+unittest
+{
+    import heartbeat: naiveDailyCalendar;
+    import std.datetime: Date;
+    import std.range: take;
+
+    auto calendar = naiveDailyCalendar(Date(2019, 1, 1), Date(2019, 1, 31));
+    auto prices = getGaussianPrices(calendar, 0., 0.05, 100.);
+    auto n = calendar.length;
+
+    import std.stdio: writeln;
+    assert(prices.front == 100., "initial price ok");
+    prices.popFront();
+
+}
